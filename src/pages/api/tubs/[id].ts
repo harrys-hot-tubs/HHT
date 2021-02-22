3fd448dbcb7108f6092dd8d950d9c879e712eca3
Max Wood <bridges.wood@gmail.com>
2021-02-22 22:10:05 +0000

    Tubs rework.

--- src/pages/api/tubs/[id].ts
+++ src/pages/api/tubs/[id].ts
@@ -1,23 +1,32 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { TubDB } from '../../../typings/Tub'
-import db from '../../../utils/db'
+import { PriceResponse } from '@typings/api/Checkout'
+import { ConnectedRequest } from '@typings/api/Request'
+import { LocationDB } from '@typings/Location'
+import { TubDB } from '@typings/Tub'
+import { stringToMoment } from '@utils/date'
+import db from '@utils/db'
+import Knex from 'knex'
+import moment from 'moment'
+import { NextApiResponse } from 'next'
 
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
-) {
+async function handler(req: ConnectedRequest, res: NextApiResponse) {
 	switch (req.method) {
 		case 'GET':
-			return await get(req, res)
+			return get(req, res)
+		case 'POST':
+			return post(req, res)
+		default:
+			res.setHeader('Allow', ['GET', 'POST'])
+			res.status(405).end('Method not allowed.')
 	}
 }
 
-const get = async (req: NextApiRequest, res: NextApiResponse<TubDB>) => {
+const get = async (req: ConnectedRequest, res: NextApiResponse<TubDB>) => {
 	const {
 		query: { id },
 	} = req
 	try {
-		const tub = await db<TubDB>('tubs')
+		const tub = await req
+			.db<TubDB>('tubs')
 			.select()
 			.first()
 			.where('tub_id', '=', id)
@@ -26,3 +35,57 @@ const get = async (req: NextApiRequest, res: NextApiResponse<TubDB>) => {
 		return res.status(400).json(e)
 	}
 }
+
+const post = async (
+	req: ConnectedRequest,
+	res: NextApiResponse<PriceResponse>
+) => {
+	const {
+		query: { id },
+		body: { startDate, endDate },
+		db,
+	} = req
+	try {
+		const tubID = Number(id as string)
+		const parsedStartDate = stringToMoment(startDate)
+		const parsedEndDate = stringToMoment(endDate)
+
+		const price = await getPrice({
+			tubID,
+			startDate: parsedStartDate,
+			endDate: parsedEndDate,
+			db,
+		})
+		return res.status(200).json({ price })
+	} catch (e) {
+		console.log('e', e)
+		return res.status(400).json(e)
+	}
+}
+
+const getPrice = async ({
+	tubID,
+	startDate,
+	endDate,
+	db,
+}: {
+	tubID: number
+	startDate: moment.Moment
+	endDate: moment.Moment
+	db: Knex
+}): Promise<number> => {
+	const difference = endDate.diff(startDate, 'days')
+	if (difference > 7 || difference < 2) throw new Error('Duration is invalid.')
+
+	const { initial_price, night_price } = await db<LocationDB>('locations')
+		.select('initial_price', 'night_price')
+		.first()
+		.join('tubs', 'tubs.location_id', '=', 'locations.location_id')
+		.where('tubs.tub_id', '=', tubID)
+
+	const finalPrice =
+		Number(initial_price) + (difference - 2) * Number(night_price)
+	return finalPrice
+}
+
+export default db()(handler)
