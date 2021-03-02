81d6077a2f1f0556cebd4f47414239e02f776f58
Max Wood <bridges.wood@gmail.com>
2021-03-02 17:07:51 +0000

    Changes to no availabilities message.

--- src/components/NoAvailabilities.tsx
+++ src/components/NoAvailabilities.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const NoAvailabilities = () => (
 	<div className='no-availabilities'>
-		<h5>Sorry, no availabilities on these dates.</h5>
+		<h5>Oops, no availabilities on availability on these dates.</h5>
+		<p>
+			We might have a solution for you though! Get in touch here at{' '}
+			<a href='tel:07554002075'>07554&nbsp;002&nbsp;075</a> for alternative
+			dates.
+		</p>
 	</div>
 )
 
