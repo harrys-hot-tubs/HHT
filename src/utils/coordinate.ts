8c121a1c01d79da0a31026a96d074445a1fa97bd
Max Wood <bridges.wood@gmail.com>
2021-03-06 14:38:35 +0000

    Changes to distance calulcation.

--- src/utils/coordinate.ts
+++ src/utils/coordinate.ts
@@ -18,16 +18,19 @@ export class Coordinate {
 		const { latitude: lat2, longitude: long2 } = location
 		const theta1 = this.toRads(lat1)
 		const theta2 = this.toRads(lat2)
-		const delta_theta = this.toRads(lat2 - lat1)
-		const delta_lambda = this.toRads(long2 - long1)
+		const delta_theta = theta2 - theta1
+		const lambda1 = this.toRads(long1)
+		const lambda2 = this.toRads(long2)
+		const delta_lambda = lambda2 - lambda1
 
-		const a = Math.sin(delta_theta / 2) * Math.sin(delta_theta / 2)
-		Math.cos(theta1) *
-			Math.cos(theta2) *
-			Math.sin(delta_lambda / 2) *
-			Math.sin(delta_lambda / 2)
+		const a =
+			Math.sin(delta_theta / 2) * Math.sin(delta_theta / 2) +
+			Math.cos(theta1) *
+				Math.cos(theta2) *
+				Math.sin(delta_lambda / 2) *
+				Math.sin(delta_lambda / 2)
 
-		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+		const c = 2 * Math.atan(Math.sqrt(a))
 		const d = Coordinate.R * c
 
 		return d
