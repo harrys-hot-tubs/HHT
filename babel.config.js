4159f2e4e9157d9bb1311de5c7750ecc83821884
Max Wood <bridges.wood@gmail.com>
2021-03-16 19:59:21 +0000

    Changes to babel.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  "presets": [
+    "next/babel",
+    "@babel/preset-react"
+  ],
   "env": {
     "test": {
       "presets": [
@@ -6,11 +10,6 @@ module.exports = {
         "next/babel"
       ]
     },
-    "development": {
-      "presets": [
-        "next/babel",
-        "@babel/preset-react"
-      ]
-    }
-  }
+  },
+  "plugins": ["@babel/plugin-proposal-class-properties"]
 }
