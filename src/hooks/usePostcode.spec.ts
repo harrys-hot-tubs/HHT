52eefc2ee8278aa55cab1320506d328fec43dde0
Max Wood <bridges.wood@gmail.com>
2021-03-28 18:51:05 +0100

    Add postcode tests.

---
+++ src/hooks/usePostcode.spec.ts
@@ -0,0 +1,62 @@
+import usePostcode from '@hooks/usePostcode'
+import { act, renderHook } from '@testing-library/react-hooks'
+
+const name = 'test'
+
+afterEach(() => {
+	localStorage.clear()
+})
+
+it('accesses stored data if available', () => {
+	const storedValue = 'test'
+	localStorage.setItem(name, storedValue)
+
+	const { result } = renderHook(() => usePostcode(name))
+	const { value } = result.current
+	expect(value).toBe(storedValue)
+})
+
+//TODO finish these tests refer to useAsyncValidatedInput
+
+it('defaults to the fallback if no data is available', () => {
+	const { result } = renderHook(() => usePostcode(name))
+	const [value] = result.current
+	expect(value).toBe('')
+})
+
+it('sets stored data when updated', () => {
+	const storedValue = 'test'
+
+	const { result } = renderHook(() => usePostcode(name))
+	act(() => {
+		result.current[1](storedValue)
+	})
+
+	expect(localStorage.setItem).toHaveBeenLastCalledWith(name, storedValue)
+})
+
+it('sets active data when updated', () => {
+	const storedValue = 'test'
+
+	const { result } = renderHook(() => usePostcode(name))
+	act(() => {
+		result.current[1](storedValue)
+	})
+
+	expect(result.current[0]).toBe(storedValue)
+})
+
+it('overwrites stored data when updated', () => {
+	const storedValue = 'test'
+	const nextValue = 'next'
+	localStorage.setItem(name, storedValue)
+
+	const { result } = renderHook(() => usePostcode(name))
+	expect(result.current[0]).toBe(storedValue)
+	act(() => {
+		result.current[1](nextValue)
+	})
+
+	expect(localStorage.setItem).toHaveBeenLastCalledWith(name, nextValue)
+	expect(result.current[0]).toBe(nextValue)
+})
